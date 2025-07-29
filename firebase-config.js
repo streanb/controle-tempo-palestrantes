5f850ea39445e027445146e979e120e0a8b902fc
streanb <curriculonuno@gmail.com>
2025-07-29 22:25:36 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,9 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyDySoLa1TbiD5-oVgg5oWrOTOp-l8x8qVE",
   authDomain: "controlo-tempo.firebaseapp.com",
-  databaseURL: "https://controlo-tempo-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://controlo-tempo.firebaseio.com", // CORRIGIDO AQUI
   projectId: "controlo-tempo",
   storageBucket: "controlo-tempo.appspot.com",
   messagingSenderId: "727603507122",
   appId: "1:727603507122:web:4e7c81136580a37dc83eab"
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
