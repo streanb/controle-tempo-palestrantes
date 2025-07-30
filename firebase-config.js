a6f6a8316eaf5ac12de51d3cb2fdb260800d1411
streanb <curriculonuno@gmail.com>
2025-07-30 15:07:24 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDySoLa1TbiD5-oVgg5oWrOTOp-l8x8qVE",
-  authDomain: "controlo-tempo.firebaseapp.com",
-  databaseURL: "https://controlo-tempo-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "controlo-tempo",
-  storageBucket: "controlo-tempo.appspot.com",
-  messagingSenderId: "727603507122",
-  appId: "1:727603507122:web:4e7c81136580a37dc83eab"
-};
