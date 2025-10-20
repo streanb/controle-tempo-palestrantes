33abc927ded3ca108b339bbb6ab892c492cdbea0
streanb <curriculonuno@gmail.com>
2025-10-20 21:46:08 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDySoLa1TbiD5-oVgg5oWrOTOp-l8x8qVE",
-  authDomain: "controlo-tempo.firebaseapp.com",
-  databaseURL: "https://controlo-tempo-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "controlo-tempo",
-  storageBucket: "controlo-tempo.appspot.com",
+  apiKey: "SUA_API_KEY",
+  authDomain: "contolotempo.firebaseapp.com",
+  databaseURL: "https://contolotempo-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "contolotempo",
+  storageBucket: "contolotempo.appspot.com",
   messagingSenderId: "727603507122",
   appId: "1:727603507122:web:4e7c81136580a37dc83eab"
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
